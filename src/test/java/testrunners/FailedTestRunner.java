7fc5ec4ab00d62bb35540d31012c95be38c836ea
Vinothini R <shanvino.012710@gmail.com>
2023-09-10 12:23:56 -0400

    Adding Class Module

--- src/test/java/testrunners/FailedTestRunner.java
+++ src/test/java/testrunners/FailedTestRunner.java
@@ -5,9 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//dryrun will see mapping between feature file and step definition
-//html, xml, json, junit, extent
-@CucumberOptions(features="@target/failed_scenarios.txt", glue="stepdefinitions",
+/*@CucumberOptions(features="@target/failed_scenarios.txt", glue="stepdefinitions",
 monochrome=true,
 plugin= {"html:target/cucumber.html","json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		"rerun:target/failed_scenarios.txt"})
@@ -17,4 +15,17 @@ public class FailedTestRunner extends AbstractTestNGCucumberTests{
 	public Object[][] scenarios(){
 		return super.scenarios();
 	}
+	
+}*/
+@CucumberOptions(features={"src/test/resources/features/Class"}, glue={"stepdefinitions"},
+monochrome=true, dryRun=true,
+plugin= {"pretty","html:target/cucumber.html","json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"rerun:target/failed_scenarios.txt"})
+public class FailedTestRunner extends AbstractTestNGCucumberTests{
+	@Override
+	@DataProvider(parallel=true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
+	
 }
---
+++ src/test/java/testrunners/TestRunner.java
@@ -0,0 +1,20 @@
+package testrunners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	  plugin = {"pretty", "html:target/cucumber.html"},
+	  features = "src/test/resources/features/Class", 
+	  glue= {"stepDefinations"},monochrome=true)
+
+public class TestRunner {
+	
+ 
+}
+
